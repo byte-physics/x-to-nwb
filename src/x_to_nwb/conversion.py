b0402952fa75c1986a718df029715ce8394466d1
Thomas Braun <thomas.braun@byte-physics.de>
2020-12-06 21:48:03 +0100

    src/x_to_nwb/conversion.py: Support ABFv1 file as well

--- src/x_to_nwb/conversion.py
+++ src/x_to_nwb/conversion.py
@@ -2,7 +2,9 @@ import os
 import argparse
 import logging
 
+import pyabf
 from .ABF2Converter import ABF2Converter
+from .ABF1Converter import ABF1Converter
 from .DatConverter import DatConverter
 
 
@@ -19,12 +21,14 @@ def convert(
     searchSettingsFile=True,
     includeChannelList="*",
     discardChannelList=None,
+    acquisitionChannelName=None,
+    stimulusChannelName=None,
 ):
     """
     Convert the given file to a NeuroDataWithoutBorders file using pynwb
 
     Supported fileformats:
-        - ABF v2 files created by Clampex
+        - ABF v1/v2 files created by Clampex
         - DAT files created by Patchmaster v2x90
 
     :param inFileOrFolder: path to a file or folder
@@ -32,11 +36,13 @@ def convert(
     :param fileType: file type to be converted, must be passed iff `inFileOrFolder` refers to a folder
     :param outputMetadata: output metadata of the file, helpful for debugging
     :param multipleGroupsPerFile: Write all Groups in the DAT file into one NWB
-                                  file. By default we create one NWB per Group (ignored for ABF files).
-    :param searchSettingsFile: Search the JSON amplifier settings file and warn if it could not be found (ignored for DAT files)
+                                  file. By default we create one NWB per Group (DAT files only).
+    :param searchSettingsFile: Search the JSON amplifier settings file and warn if it could not be found (ABFv2 only)
     :param compression: Toggle compression for HDF5 datasets
-    :param includeChannelList: ADC channels to write into the NWB file (ignored for DAT files)
-    :param discardChannelList: ADC channels to not write into the NWB file (ignored for DAT files)
+    :param includeChannelList: ADC channels to write into the NWB file (ABFv2 only)
+    :param discardChannelList: ADC channels to not write into the NWB file (ABFv2 only)
+    :param acquisitionChannelName: Output only that channel as acquisition channel (ABFv1 only)
+    :param stimulusChannelName: Output only that channel as stimulation channel (ABFv1 only)
 
     :return: path of the created NWB file
     """
@@ -46,6 +52,13 @@ def convert(
 
     if os.path.isfile(inFileOrFolder):
         root, ext = os.path.splitext(inFileOrFolder)
+
+        # determine specific ABF major version
+        if ext == ".abf":
+            abf = pyabf.ABF(inFileOrFolder)
+            major_version = abf.abfVersion["major"]
+            ext = f".abfv{major_version}"
+
     if os.path.isdir(inFileOrFolder):
         if not fileType:
             raise ValueError("Missing fileType when passing a folder")
@@ -64,7 +77,19 @@ def convert(
         else:
             raise ValueError(f"The output file {outFile} does already exist.")
 
-    if ext == ".abf":
+    if ext == ".abfv1":
+        if outputMetadata:
+            ABF1Converter.outputMetadata(inFileOrFolder)
+        else:
+            conv = ABF1Converter(
+                inFileOrFolder,
+                outFile,
+                compression=compression,
+                stimulusChannelName=stimulusChannelName,
+                acquisitionChannelName=acquisitionChannelName,
+            )
+            conv.convert()
+    elif ext == ".abfv2":
         if outputMetadata:
             ABF2Converter.outputMetadata(inFileOrFolder)
         else:
@@ -96,6 +121,8 @@ def convert_cli():
         title="Common", description="Options which are applicable to both ABF and DAT files"
     )
     abf_group = parser.add_argument_group(title="ABF", description="Options which are applicable to ABF")
+    abfv1_group = parser.add_argument_group(title="ABFv1", description="Options which are applicable to ABFv1")
+    abfv2_group = parser.add_argument_group(title="ABFv2", description="Options which are applicable to ABFv2")
     dat_group = parser.add_argument_group(title="DAT", description="Options which are applicable to DAT")
 
     feature_parser = common_group.add_mutually_exclusive_group(required=False)
@@ -116,18 +143,23 @@ def convert_cli():
     )
     common_group.add_argument("--log", type=str, help="Log level for debugging, defaults to the root logger's value.")
     common_group.add_argument("filesOrFolders", nargs="+", help="List of files/folders to convert.")
-
-    abf_group.add_argument(
-        "--protocolDir", type=str, help=("Disc location where custom waveforms in ATF format are stored.")
-    )
     abf_group.add_argument(
         "--fileType",
         type=str,
         default=None,
-        choices=[".abf"],
+        choices=[".abfv1", ".abfv2"],
         help=("Type of the files to convert (only required if passing folders)."),
     )
-    abf_group.add_argument(
+
+    abfv1_group.add_argument(
+        "--acquisitionChannelName", default=None, help="Output only the given channel as acquisition."
+    )
+    abfv1_group.add_argument("--stimulusChannelName", default=None, help="Output only the given channel as stimulus.")
+
+    abfv2_group.add_argument(
+        "--protocolDir", type=str, help=("Disc location where custom waveforms in ATF format are stored.")
+    )
+    abfv2_group.add_argument(
         "--no-searchSettingsFile",
         action="store_false",
         dest="searchSettingsFile",
@@ -135,15 +167,15 @@ def convert_cli():
         help="Don't search the JSON file for the amplifier settings.",
     )
 
-    abf_group_channels = abf_group.add_mutually_exclusive_group(required=False)
-    abf_group_channels.add_argument(
+    abfv2_group_channels = abfv2_group.add_mutually_exclusive_group(required=False)
+    abfv2_group_channels.add_argument(
         "--includeChannel",
         type=str,
         dest="includeChannelList",
         action="append",
         help=f"Name of ADC channels to include in the NWB file.",
     )
-    abf_group_channels.add_argument(
+    abfv2_group_channels.add_argument(
         "--discardChannel",
         type=str,
         dest="discardChannelList",
@@ -187,6 +219,8 @@ def convert_cli():
             searchSettingsFile=args.searchSettingsFile,
             includeChannelList=args.includeChannelList,
             discardChannelList=args.discardChannelList,
+            acquisitionChannelName=args.acquisitionChannelName,
+            stimulusChannelName=args.stimulusChannelName,
         )
 
 
